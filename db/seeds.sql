73f3aeaab1eb1cca47fd5cfc1ca7b27e0858d28d
Allison Haver <allisonbhaver@gmail.com>
2020-10-17 22:18:29 -0400

    added files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
+INSERT INTO burgers (burger_name) VALUES ("Vegan Bliss Burger");
+INSERT INTO burgers (burger_name) VALUES ("Everything Bacon Burger");
